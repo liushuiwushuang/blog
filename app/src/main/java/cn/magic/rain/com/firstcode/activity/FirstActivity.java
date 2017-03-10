b18c9d181970e501624cbd1c12eea3d5e566481c
magicRain <1303637389@qq.com>
2017-03-10 19:25:09 +0800

    remove unused package

--- app/src/main/java/cn/magic/rain/com/firstcode/activity/FirstActivity.java
+++ app/src/main/java/cn/magic/rain/com/firstcode/activity/FirstActivity.java
@@ -1,9 +1,8 @@
 package cn.magic.rain.com.firstcode.activity;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
